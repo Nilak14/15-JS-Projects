c9ac9862da7d3f8ca7b7a79d0c4da32ef513f4de
Nilak <nilakpathak14@gmail.com>
2023-10-07 21:20:22 +0545

    ,

--- 13-Gallery/app.js
+++ 13-Gallery/app.js
@@ -17,18 +17,30 @@ class Gallery {
         this.closeBtn = getElement('.close-btn')
         this.nextBtn = getElement('.next-btn')
         this.prevBtn = getElement('.prev-btn')
+        this.closeModal = this.closeModal.bind(this)
+        this.prevImage = this.prevImage.bind(this)
+        this.nextImage = this.nextImage.bind(this)
+        this.selectImageFromList = this.selectImageFromList.bind(this)
         this.container.addEventListener('click', function (e) {
             if (e.target.classList.contains('img')) {
                 this.openModal(e.target, this.list);
             }
         }.bind(this));
-        this.closeBtn.addEventListener('click', this.closeModal.bind(this))
     }
 
     openModal(selectedImage, list) {
-        console.log(list)
         this.modal.classList.add('open')
         this.setMainImage(selectedImage)
+        this.modalImages.innerHTML = list.map(item => {
+            return `<img src="${item.src}" title="${item.title}" alt="${item.alt}" class="${item.dataset.id === selectedImage.dataset.id ? 'modal-img active' : 'modal-img'}" data-id="${item.dataset.id}">`
+        }).join('')
+        this.modalImages.querySelector('.active').scrollIntoView({ behavior: 'smooth' })
+        this.closeBtn.addEventListener('click', this.closeModal)
+        this.prevBtn.addEventListener('click', this.prevImage)
+        this.nextBtn.addEventListener('click', this.nextImage)
+        this.modalImages.addEventListener('click', this.selectImageFromList)
+
+
     }
 
     setMainImage(mainImage) {
@@ -37,7 +49,41 @@ class Gallery {
     }
     closeModal() {
         this.modal.classList.remove('open')
+        this.closeBtn.removeEventListener('click', this.closeModal)
+        this.prevBtn.removeEventListener('click', this.prevImage)
+        this.nextBtn.removeEventListener('click', this.nextImage)
+        this.modalImages.removeEventListener('click', this.selectImageFromList)
+
     }
+    prevImage() {
+        const selectedImage = this.modalImages.querySelector('.active');
+        const prevImage = selectedImage.previousElementSibling || this.modalImages.lastElementChild;
+        this.setMainImage(prevImage)
+        prevImage.scrollIntoView({ behavior: 'smooth' })
+        selectedImage.classList.remove('active')
+        prevImage.classList.add('active');
+    }
+
+    nextImage() {
+        const selectedImage = this.modalImages.querySelector('.active');
+        const nextImage = selectedImage.nextElementSibling || this.modalImages.firstElementChild;
+        this.setMainImage(nextImage)
+        nextImage.scrollIntoView({ behavior: 'smooth' })
+        selectedImage.classList.remove('active')
+        nextImage.classList.add('active');
+    }
+
+    selectImageFromList(e) {
+        console.log('select')
+        if (e.target.classList.contains('modal-img')) {
+            let active = this.modalImages.querySelector('.active');
+            active.classList.remove('active')
+            this.setMainImage(e.target)
+            e.target.classList.add('active')
+        }
+
+    }
+
 }
 
 const nature = new Gallery(getElement('.nature'))
